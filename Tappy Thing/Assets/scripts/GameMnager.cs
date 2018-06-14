6d19cfc97f20387a2b9274f6ef59e1ae43be0a08
Alex Griffiths <alex.griffiths96@gmail.com>
2018-06-14 11:52:54 +0100

    project upload

---
+++ Tappy Thing/Assets/scripts/GameMnager.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMnager : MonoBehaviour {
+
+	public delegate void GameDelegate();
+	public static event GameDelegate OnGameStarted;
+	public static event GameDelegate OnGameOverConfirmed;
+
+	public static GameMnager Instance;
+
+	public GameObject startPage;
+	public GameObject gameOverPage;
+	public GameObject countDownPage;
+	public Text scoreText;
+	public GameObject player;
+
+	enum PageState
+	{
+		None,
+		Start,
+		GameOver,
+		Countdown
+	}
+
+	int score = 0;
+	bool gameOver = true;
+
+	public bool GameOver { get { return gameOver; } }
+	public int Score { get { return score; } }
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	private void OnEnable()
+	{
+		CountDown.OnCountdownFinished += OnCountdownFinished;
+		TapController.OnPlayerDied += OnPlayerDied;
+		TapController.OnPlayerScored += OnPlayerScored;
+	}
+
+	private void OnDisable()
+	{
+		CountDown.OnCountdownFinished -= OnCountdownFinished;
+		TapController.OnPlayerScored -= OnPlayerScored;
+		TapController.OnPlayerDied -= OnPlayerDied;
+	}
+
+	void OnCountdownFinished()
+	{
+		SetPageState(PageState.None);
+		OnGameStarted(); //sent to tap controller
+		score = 0;
+		gameOver = false;
+	}
+
+	void OnPlayerDied()
+	{
+		gameOver = true;
+		int savedScore = PlayerPrefs.GetInt("HighScore");
+		if(score > savedScore)
+		{
+			PlayerPrefs.SetInt("HighScore", score);
+		}
+		SetPageState(PageState.GameOver);
+	}
+
+	void OnPlayerScored()
+	{
+		score++;
+		scoreText.text = score.ToString();
+	}
+
+	void SetPageState(PageState state)
+	{
+		switch (state)
+		{
+			case PageState.None:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(false);
+				countDownPage.SetActive(false);
+				break;
+
+			case PageState.Start:
+				startPage.SetActive(true);
+				gameOverPage.SetActive(false);
+				countDownPage.SetActive(false);
+				break;
+
+			case PageState.GameOver:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(true);
+				countDownPage.SetActive(false);
+				break;
+
+			case PageState.Countdown:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(false);
+				countDownPage.SetActive(true);
+				break;
+		}
+
+		
+	}
+
+	public void ConfirmGameOver()
+	{
+		//activated when replay
+		OnGameOverConfirmed();//event sent to tap controller
+		scoreText.text = "0";
+		SetPageState(PageState.Start);
+	}
+
+	public void StartGame()
+	{
+		//activated when play button hit at begining
+		SetPageState(PageState.Countdown);
+		Vector3 playerPos = player.GetComponent<TapController>().startPos;
+		player.transform.position = playerPos;
+	}
+}
---
+++ Tappy Thing/Assets/scripts/TapController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour {
+
+	public delegate void PlayerDelegate();
+	public static event PlayerDelegate OnPlayerDied;
+	public static event PlayerDelegate OnPlayerScored;
+
+	public float tapForce = 10;
+	public float tiltSmooth = 5;
+	public Vector3 startPos;
+
+	Rigidbody2D rb;
+	Quaternion downRotation;
+	Quaternion forwardRotaion;
+
+	GameMnager game;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		downRotation = Quaternion.Euler(0, 0, -90);
+		forwardRotaion = Quaternion.Euler(0, 0, 35);
+		game = GameMnager.Instance;
+		rb.simulated = false;
+	}
+
+	private void OnEnable()
+	{
+		GameMnager.OnGameStarted += OnGameStarted;
+		GameMnager.OnGameOverConfirmed += OnGameOverConfirmed;
+	}
+
+	private void OnDisable()
+	{
+		GameMnager.OnGameStarted -= OnGameStarted;
+		GameMnager.OnGameOverConfirmed -= OnGameOverConfirmed;
+	}
+
+	void OnGameStarted ()
+	{
+		rb.velocity = Vector3.zero;
+		rb.simulated = true;
+	}
+
+	void OnGameOverConfirmed()
+	{
+		transform.position = startPos;
+		transform.rotation = Quaternion.identity;
+
+	}
+
+	private void Update()
+	{
+		if (game.GameOver) return;
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			transform.rotation = forwardRotaion;
+			rb.velocity = Vector3.zero;
+			rb.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+		}
+
+		transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+	}
+
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "ScoreZone")
+		{
+			//score
+			OnPlayerScored();//event sent to gamemanger;
+			//play sound
+		}
+
+		if(col.gameObject.tag == "DeadZone")
+		{
+			rb.simulated = false;
+			//register dead event
+			OnPlayerDied(); //event sent to gamemanger;
+			//play sound
+		}
+	}
+
+}
